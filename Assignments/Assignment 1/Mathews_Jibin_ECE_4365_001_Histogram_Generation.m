bc8b2adf5ce641575ca78fc10507afdadfdadfd5
jibinmathews8 <jibinmathews8@gmail.com>
2023-02-09 08:47:12 -0600

    Create Mathews_Jibin_ECE_4365_001_Histogram_Generation.m

---
+++ Assignments/Assignment 1/Mathews_Jibin_ECE_4365_001_Histogram_Generation.m
@@ -0,0 +1,17 @@
+%{
+Jibin Mathews
+ECE 4365 001
+Assignment 1: Histogram Generation
+
+Description: 
+1) histogram of values from attached data set with appropriate number of bins
+
+2) mean and standard deviation associated with these data
+
+3) Gaussian distribution that most closely describes these data
+
+4) Plot of histogram with cumulative probability curve
+
+5) Overlay of Gaussian curve from #3 with plot from #4
+
+%}
